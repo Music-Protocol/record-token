1e8097760fb30048fe85f3a49e92caed1c1203b3
simonemastella <simonemastella@gmail.com>
2022-12-28 11:35:05 +0100

    updated compiler to avoid false positive Math.sol

--- contracts/JTPManagement.sol
+++ contracts/JTPManagement.sol
@@ -1,15 +1,25 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/access/AccessControl.sol"; //to mint and burn
 //import interface of JTP(erc20) to call mint and burn
+
+//the interfaces of JTP that we need to call on 
+interface IJTP{
+    function mint(address to, uint256 amount) external;
+    function burn(uint256 amount) external;
+}
 
 contract JTPManagement is AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    constructor() {
+    IJTP private jtp;
+
+    constructor(address _jtp) {
+        //set jtp
+        jtp = IJTP(_jtp);
         // Grant the minter role to a specified account
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.1",
+  solidity: "0.8.2",
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
