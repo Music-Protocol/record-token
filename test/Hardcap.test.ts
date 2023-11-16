958f800b66e5ddb1a1af4fc083810444f62379f0
agreco <agreco@efebia.com>
2023-11-16 18:32:38 +0100

    Hardcap test

---
+++ test/Hardcap.test.ts
@@ -0,0 +1,38 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { FanToArtistStaking, Web3MusicNativeToken } from '../typechain-types/index';
+import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs';
+import { timeMachine } from './utils/utils';
+import { BigNumber } from 'ethers';
+
+describe("Hardcap", function () {
+    
+    async function deployToken() {
+        const [owner, addr1, addr2, artist1, artist2] = await ethers.getSigners();
+        const defVeReward = 10;
+        const defArtistReward = 10;
+
+        const FTAS = await ethers.getContractFactory('FanToArtistStaking');
+        const fanToArtistStaking = await FTAS.deploy();
+        await fanToArtistStaking.deployed();
+
+        const cWeb3MusicNativeToken = await ethers.getContractFactory('Web3MusicNativeToken');
+        const Web3MusicNativeToken = await cWeb3MusicNativeToken.deploy(fanToArtistStaking.address) as Web3MusicNativeToken;
+        await Web3MusicNativeToken.deployed();
+        await fanToArtistStaking.initialize(Web3MusicNativeToken.address, defVeReward, defArtistReward, 10, 86400);
+
+        return { Web3MusicNativeToken, fanToArtistStaking, owner, addr1, addr2, artist1, artist2 }
+    }
+
+    it('It is not possible to mint more than one billion tokens', async () => {
+        const { Web3MusicNativeToken, fanToArtistStaking, owner, addr1, artist1  } = await loadFixture(deployToken);
+
+        await expect(Web3MusicNativeToken.connect(owner).mint(owner.address, 1000000000000000000000000000n)).emit(Web3MusicNativeToken, "Transfer");
+
+        await expect(Web3MusicNativeToken.connect(owner).mint(owner.address, 1))
+            .to.be.revertedWith("W3T: Maximum limit of minable tokens reached") 
+    });
+
+});
